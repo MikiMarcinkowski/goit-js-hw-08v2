666cf055960fb07026194e881e1007c802149c20
mikus151 <mikus151@wp.pl>
2023-06-19 00:18:30 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const storageTimeKey = 'videoplayer-current-time';
+
+const onPlay = function (timeUpdate) {
+  localStorage.setItem(storageTimeKey, timeUpdate.seconds);
+};
+
+player.on('play', onPlay);
+
+const actualTime = localStorage.getItem(storageTimeKey);
+
+player
+  .setCurrentTime(actualTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
