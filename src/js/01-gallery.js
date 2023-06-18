2fe3298472941e8a69e66cd2a78cb3d3de6ac04e
mikus151 <mikus151@wp.pl>
2023-06-18 20:12:06 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,39 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+const galeryItem = galleryItems
+  .map(
+    item =>
+      `
+      <li>
+        <a class="gallery__item" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>
+    
+      `
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galeryItem);
+
+gallery.style.listStyleType = 'none'; 
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  enableKeyboard: true,
+});
